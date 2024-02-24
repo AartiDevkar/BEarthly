29ba23239f4bd78ba6629ce3bf9a5f188d9e6708
Aarti Devkar <aartideokar2003@gmail.com>
2024-02-24 21:06:58 +0530

    feat:data model for ngo cards

---
+++ lib/connect_pages/data/ngos_data.dart
@@ -0,0 +1,52 @@
+import 'package:bearthly/connect_pages/data/ngos_model.dart';
+
+List<NGO> ngos = [
+  NGO(
+    ngoName: 'Sample NGO 1',
+    contactPerson: 'John Doe',
+    email: 'contact@example.com',
+    phoneNumber: '+1234567890',
+    address: '123 NGO Street, City',
+    imageUrl:
+        'https://im.whatshot.in/img/2020/Jun/istock-1130655067-cropped-1591265020.jpg',
+    websiteUrl: 'https://catchfoundation.in/',
+  ),
+  NGO(
+    ngoName: 'Sample NGO 2',
+    contactPerson: 'Jane Smith',
+    email: 'info@samplengo.org',
+    phoneNumber: '+9876543210',
+    address: '456 NGO Avenue, Town',
+    imageUrl: 'https://lifesup.com.vn/wp-content/uploads/2022/10/4-min.png',
+    websiteUrl: 'https://www.greenyatra.org/',
+  ),
+  NGO(
+    ngoName: 'Sample NGO 2',
+    contactPerson: 'Jane Smith',
+    email: 'info@samplengo.org',
+    phoneNumber: '+9876543210',
+    address: '456 NGO Avenue, Town',
+    imageUrl:
+        'https://climatefactchecks.org/wp-content/uploads/2023/06/image-4-300x169.png',
+    websiteUrl: 'https://www.greenyatra.org/',
+  ),
+  NGO(
+    ngoName: 'Sample NGO 3',
+    contactPerson: 'Jane Smith',
+    email: 'info@samplengo.org',
+    phoneNumber: '+9876543210',
+    address: '456 NGO Avenue, Town',
+    imageUrl: 'https://waidy.it/img/post/how-to-reduce-pollution.jpg',
+    websiteUrl: 'https://www.greenyatra.org/',
+  ),
+  NGO(
+    ngoName: 'Sample NGO 4',
+    contactPerson: 'Jane Smith',
+    email: 'info@samplengo.org',
+    phoneNumber: '+9876543210',
+    address: '456 NGO Avenue, Town',
+    imageUrl: 'https://waidy.it/img/post/how-to-reduce-pollution.jpg',
+    websiteUrl: 'https://www.greenyatra.org/',
+  ),
+  // Add more NGO objects as needed
+];
---
+++ lib/connect_pages/data/ngos_model.dart
@@ -0,0 +1,19 @@
+class NGO {
+  final String ngoName;
+  final String contactPerson;
+  final String email;
+  final String phoneNumber;
+  final String address;
+  final String imageUrl;
+  final String websiteUrl;
+
+  NGO({
+    required this.ngoName,
+    required this.contactPerson,
+    required this.email,
+    required this.phoneNumber,
+    required this.address,
+    required this.imageUrl,
+    required this.websiteUrl,
+  });
+}
